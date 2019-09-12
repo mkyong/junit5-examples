5bb90951a34a6bacbf82315fa1917d62bce0bfa6
mkyong <mkyong2002@gmail.com>
2019-09-12 15:59:45 +0800

    init

---
+++ junit5-maven/src/main/java/com/mkyong/core/services/HelloService.java
@@ -0,0 +1,8 @@
+package com.mkyong.core.services;
+
+public interface HelloService {
+
+    String get();
+
+    String getDefault();
+}
---
+++ junit5-maven/src/main/java/com/mkyong/core/services/HelloServiceImpl.java
@@ -0,0 +1,24 @@
+package com.mkyong.core.services;
+
+import com.mkyong.core.repository.HelloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloServiceImpl implements HelloService {
+
+    @Autowired
+    HelloRepository helloRepository;
+
+    @Override
+    public String get() {
+        return helloRepository.get();
+    }
+
+    @Override
+    public String getDefault() {
+        return "hello";
+    }
+
+
+}
