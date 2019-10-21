c1f4ce231a57f7b56eb82402486b267512efe5a3
mkyong <mkyong2002@gmail.com>
2019-10-21 15:11:48 +0800

    add conditional est

---
+++ junit5-examples/src/test/java/com/mkyong/conditional/SystemPropertyTest.java
@@ -0,0 +1,30 @@
+package com.mkyong.conditional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import java.util.Properties;
+
+// https://www.mkyong.com/java/how-to-list-all-system-properties-key-and-value-in-java/
+public class SystemPropertyTest {
+
+    @Test
+    @EnabledIfSystemProperty(named = "java.vm.name", matches = ".*OpenJDK.*")
+    void onOpenJDK() {
+        System.out.println("Run this on OpenJDK!");
+    }
+
+    @Test
+    @DisabledIfSystemProperty(named = "user.country", matches = "MY")
+    void notOnCountryMalaysia() {
+        System.out.println("Do not run this on country code MY");
+    }
+
+    @Test
+    void printSystemProperties() {
+        Properties properties = System.getProperties();
+        properties.forEach((k, v) -> System.out.println(k + ":" + v));
+    }
+
+}
